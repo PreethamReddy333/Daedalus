e92e33d4ec30d473fa5a4dcdeae2f17073fbdad9
PreethamReddy333 <preetham9070@gmail.com>
2026-01-10 13:36:26 +0530

    commiting new files

---
+++ arithmetic/target/wasm32-unknown-unknown/release/deps/arithmetic.d
@@ -0,0 +1,5 @@
+/Users/preetham/Documents/demoo/arithmetic/target/wasm32-unknown-unknown/release/deps/arithmetic.d: src/lib.rs
+
+/Users/preetham/Documents/demoo/arithmetic/target/wasm32-unknown-unknown/release/deps/arithmetic.wasm: src/lib.rs
+
+src/lib.rs:
